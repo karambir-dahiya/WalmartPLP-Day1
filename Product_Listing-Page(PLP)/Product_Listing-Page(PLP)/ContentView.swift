78cfef9ceb1a1f3579463f910957d6fd6acd763a
Karambir <karmvirdahiya.com@gmail.com>
2026-06-12 18:36:00 +0530

    Initial commit with SwiftUI

---
+++ Product_Listing-Page(PLP)/Product_Listing-Page(PLP)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Product_Listing-Page(PLP)
+//
+//  Created by Siyaa Dahiya on 12/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Product_Listing-Page(PLP)/Product_Listing-Page(PLP)/Product_Listing_Page_PLP_App.swift
@@ -0,0 +1,17 @@
+//
+//  Product_Listing_Page_PLP_App.swift
+//  Product_Listing-Page(PLP)
+//
+//  Created by Siyaa Dahiya on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Product_Listing_Page_PLP_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
